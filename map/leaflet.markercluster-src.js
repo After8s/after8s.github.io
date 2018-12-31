4044930241441c8bb2ad9bf4b28dea20802e098a
stanotomic <s.stanelle@gmail.com>
2018-12-31 18:03:01 +0100

    changed clustering threshold

--- map/leaflet.markercluster-src.js
+++ map/leaflet.markercluster-src.js
@@ -10,7 +10,7 @@
 L.MarkerClusterGroup = L.FeatureGroup.extend({
 
 	options: {
-		maxClusterRadius: 80, //A cluster will cover at most this many pixels from its center
+		maxClusterRadius: 30, //A cluster will cover at most this many pixels from its center
 		iconCreateFunction: null,
 
 		spiderfyOnMaxZoom: true,
