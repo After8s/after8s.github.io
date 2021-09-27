b764979b020722a0704f83ffd5b5182549559bd7
stanotomic <s.stanelle@gmail.com>
2021-09-27 10:37:45 +0200

    Update leaflet.markercluster-src.js

--- map/leaflet.markercluster-src.js
+++ map/leaflet.markercluster-src.js
@@ -10,7 +10,7 @@
 L.MarkerClusterGroup = L.FeatureGroup.extend({
 
 	options: {
-		maxClusterRadius: 20, //A cluster will cover at most this many pixels from its center
+		maxClusterRadius: 80, //A cluster will cover at most this many pixels from its center
 		iconCreateFunction: null,
 
 		spiderfyOnMaxZoom: true,
